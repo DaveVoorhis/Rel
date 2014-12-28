a949735537f16c7e53a7d208b8e3fd628a8ebc61
dave <dave@armchair.mb.ca>
2014-12-28 18:39:53 +0000

    Remove redundant imports.

--- Rel3DBrowser/ca/mb/armchair/rel3/dbrowser/monitor/Monitor.java
+++ Rel3DBrowser/ca/mb/armchair/rel3/dbrowser/monitor/Monitor.java
@@ -8,15 +8,10 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.net.JarURLConnection;
-import java.net.MalformedURLException;
-import java.net.URISyntaxException;
-import java.net.URL;
 
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
@@ -32,7 +27,6 @@ import javax.swing.JToolBar;
 import javax.swing.SwingUtilities;
 import javax.swing.filechooser.FileNameExtensionFilter;
 
-import sun.net.www.protocol.file.FileURLConnection;
 import ca.mb.armchair.rel3.dbrowser.ui.Splash;
 import ca.mb.armchair.rel3.dbrowser.utilities.Preferences;
 
