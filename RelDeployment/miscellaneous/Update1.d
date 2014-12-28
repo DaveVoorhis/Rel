681843b6c3f138e302e089d86366d900a5153c07
dave <dave@armchair.mb.ca>
2014-12-28 15:20:46 +0000

    Remove old debugging files.

--- RelDeployment/miscellaneous/Update1.d
+++
@@ -1,5 +0,0 @@
-insert ComposedBy rel{tup{Problem# '095a', FirstName 'Sebastian', LastName 'Nowacki' }} , 
-insert Problem rel { tup { Problem# '095a' , DR 6 , Month 'January' , Year '2013' , NumberOfComposers NumberOfComposers from tuple from ( DTYprob where DTY# = 123 ) } } , 
-delete DTYcomposer where DTY# = 123,
-update DTYprob where DTY# = 123 : {NumberOfComposers := 0}, 
-insert DTYforCP rel{tup{DTY# 123, Problem# '095a'}};
