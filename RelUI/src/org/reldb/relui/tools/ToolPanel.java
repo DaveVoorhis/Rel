ab3549fa2b1c6be303c4e2a97bcf5af36321c3c0
dave <dave@armchair.mb.ca>
2015-03-08 09:18:59 +0000

    New icons and tooltips

--- RelUI/src/org/reldb/relui/tools/ToolPanel.java
+++ RelUI/src/org/reldb/relui/tools/ToolPanel.java
@@ -30,48 +30,66 @@ public class ToolPanel extends Composite {
 		
 		ToolItem item1 = new ToolItem(toolBar, SWT.PUSH);
 		item1.setImage(ResourceManager.getPluginImage("RelUI", "icons/clearIcon.png"));
+		item1.setToolTipText("Clear");
 
 		ToolItem item2 = new ToolItem(toolBar, SWT.PUSH);
 		item2.setImage(ResourceManager.getPluginImage("RelUI", "icons/safeIcon.png"));
+		item2.setToolTipText("Backup");
 
 		ToolItem item3 = new ToolItem(toolBar, SWT.PUSH);
 		item3.setImage(ResourceManager.getPluginImage("RelUI", "icons/saveIcon.png"));
+		item3.setToolTipText("Save");
 
 		ToolItem item4 = new ToolItem(toolBar, SWT.PUSH);
 		item4.setImage(ResourceManager.getPluginImage("RelUI", "icons/saveTextIcon.png"));
+		item4.setToolTipText("Save as text");
 
 		ToolItem item5 = new ToolItem(toolBar, SWT.PUSH);
 		item5.setImage(ResourceManager.getPluginImage("RelUI", "icons/saveHTMLIcon.png"));
+		item5.setToolTipText("Save as HTML");
 
 		ToolItem item6 = new ToolItem(toolBar, SWT.PUSH);
 		item6.setImage(ResourceManager.getPluginImage("RelUI", "icons/saveHistoryIcon.png"));
+		item6.setToolTipText("Save history");
 
 		ToolItem item7 = new ToolItem(toolBar, SWT.PUSH);
 		item7.setImage(ResourceManager.getPluginImage("RelUI", "icons/loadIcon.png"));
+		item7.setToolTipText("Load");
 
 		ToolItem item8 = new ToolItem(toolBar, SWT.PUSH);
 		item8.setImage(ResourceManager.getPluginImage("RelUI", "icons/pathIcon.png"));
+		item8.setToolTipText("Insert file path");
 
 		ToolItem item9 = new ToolItem(toolBar, SWT.PUSH);
 		item9.setImage(ResourceManager.getPluginImage("RelUI", "icons/copyToInputIcon.png"));
+		item9.setToolTipText("Copy output to input");
 
 		new ToolItem(toolBar, SWT.SEPARATOR);
 		
 		ToolItem item10 = new ToolItem(toolBar, SWT.CHECK);
 		item10.setImage(ResourceManager.getPluginImage("RelUI", "icons/copyToOutputIcon.png"));
+		item10.setToolTipText("Copy input to output");
 		item10.setSelection(true);
 		
 		ToolItem item11 = new ToolItem(toolBar, SWT.CHECK);
 		item11.setImage(ResourceManager.getPluginImage("RelUI", "icons/wrapIcon.png"));
+		item11.setToolTipText("Wrap text");
 		item11.setSelection(true);
 		
 		ToolItem item12 = new ToolItem(toolBar, SWT.CHECK);
 		item12.setImage(ResourceManager.getPluginImage("RelUI", "icons/autoclearIcon.png"));
+		item12.setToolTipText("Automatically clear output");
 		item12.setSelection(true);
 				
 		ToolItem item13 = new ToolItem(toolBar, SWT.CHECK);
 		item13.setImage(ResourceManager.getPluginImage("RelUI", "icons/enhancedIcon.png"));
+		item13.setToolTipText("Display enhanced output");
 		item13.setSelection(true);
+		
+		ToolItem item14 = new ToolItem(toolBar, SWT.CHECK);
+		item14.setImage(ResourceManager.getPluginImage("RelUI", "icons/showOkIcon.png"));
+		item14.setToolTipText("Write 'Ok.' after execution");
+		item14.setSelection(true);
 				
 		ToolBar rightBar = new ToolBar(this, SWT.NONE);
 		FormData fd_rightBar = new FormData();
