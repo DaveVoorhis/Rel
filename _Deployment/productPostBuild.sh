660f962f56c38edee942563f9300eea245847a07
dave <dave@armchair.mb.ca>
2015-03-09 11:14:33 +0000

    Create script to run after building product

---
+++ _Deployment/productPostBuild.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cp -R ~/Documents/JavaJREs/jre-8u40-linux-i586 ~/git/Rel/_Deployment/product/linux.gtk.x86/Rel/jre
+cp -R ~/Documents/JavaJREs/jre-8u40-linux-x64 ~/git/Rel/_Deployment/product/linux.gtk.x86_64/Rel/jre
+cp -R ~/Documents/JavaJREs/jre-8u40-macosx-x64/Contents/Home ~/git/Rel/_Deployment/product/macosx.cocoa.x86_64/Rel/jre
+cp -R ~/Documents/JavaJREs/jre-8u40-windows-i586 ~/git/Rel/_Deployment/product/win32.win32.x86/Rel/jre
+cp -R ~/Documents/JavaJREs/jre-8u40-windows-x64 ~/git/Rel/_Deployment/product/win32.win32.x86_64/Rel/jre
