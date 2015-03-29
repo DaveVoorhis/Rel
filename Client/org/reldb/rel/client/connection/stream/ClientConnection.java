505c138581d48e836d6bded8c9c82de8cfdf1720
dave <dave@armchair.mb.ca>
2015-03-30 00:02:01 +0100

    Remove unused interface.

--- Client/org/reldb/rel/client/connection/stream/ClientConnection.java
+++
@@ -1,9 +0,0 @@
-package org.reldb.rel.client.connection.stream;
-
-import java.io.*;
-
-import org.reldb.rel.client.connection.Client;
-
-public interface ClientConnection extends Client {
-	public InputStream getServerResponseInputStream() throws IOException;
-}
