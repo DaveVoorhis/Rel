68dfad398d8391096f9842d54a4a46393d2360c2
dave <dave@armchair.mb.ca>
2015-04-19 15:03:41 +0100

    Set default database name to DefaultRelDb in standalone server.

--- ServerV0000/src/org/reldb/rel/v0/interpreter/Instance.java
+++ ServerV0000/src/org/reldb/rel/v0/interpreter/Instance.java
@@ -190,7 +190,7 @@ public class Instance {
 	}
 	
 	private Instance(String args[]) {
-		String databasePath = "./";
+		String databasePath = "./DefaultRelDb";
 		if (args.length >= 1) {
 			for (int i=0; i<args.length; i++) {
 				if (args[i].startsWith("-D")) {
