1035aaec6a6f083e918ef1b490b6936ce633551e
dave <dave@armchair.mb.ca>
2015-06-22 22:56:10 +0100

    Remove redundant code.

--- RelUI/src/org/reldb/dbrowser/ui/content/rel/var/RelvarEditorToolbar.java
+++ RelUI/src/org/reldb/dbrowser/ui/content/rel/var/RelvarEditorToolbar.java
@@ -1,7 +1,5 @@
 package org.reldb.dbrowser.ui.content.rel.var;
 
-import java.util.Vector;
-
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
@@ -27,8 +25,6 @@ public class RelvarEditorToolbar {
 	
 	public RelvarEditorToolbar(Composite parent, RelvarEditor relvarEditor) {
 		toolBar = new ToolBar(parent, SWT.None);
-
-		addAdditionalItems(toolBar);
 		
 		refreshBtn = new ToolItem(toolBar, SWT.PUSH);
 		refreshBtn.setToolTipText("Refresh");
@@ -68,34 +64,11 @@ public class RelvarEditorToolbar {
 		Preferences.addPreferenceChangeListener(PreferencePageGeneral.LARGE_ICONS, preferenceChangeListener);
 	}
 
-	private static class ToolbarItem {
-		private ToolItem toolItem;
-		private String iconName;
-		public ToolbarItem(ToolItem toolItem, String iconName) {
-			this.toolItem = toolItem;
-			this.iconName = iconName;
-		}
-		ToolItem getToolItem() {return toolItem;}
-		String getIconName() {return iconName;}
-	}
-	
-	private Vector<ToolbarItem> additionalItems = new Vector<ToolbarItem>();
-	
-	/** Add an additional toolbar item. */
-	protected void addAdditionalItem(ToolItem item, String iconName) {
-		additionalItems.add(new ToolbarItem(item, iconName));
-	}
-	
-	/** Override to add additional toolbar items before the default items. */
-	protected void addAdditionalItems(ToolBar toolBar) {}
-
 	public void dispose() {
 		Preferences.removePreferenceChangeListener(PreferencePageGeneral.LARGE_ICONS, preferenceChangeListener);
 	}
 
 	private void setupIcons() {
-		for (ToolbarItem tbi: additionalItems)
-			tbi.getToolItem().setImage(IconLoader.loadIcon(tbi.getIconName()));
 		refreshBtn.setImage(IconLoader.loadIcon("arrow_refresh"));
 		goToInsertBtn.setImage(IconLoader.loadIcon("table_row_insert"));
 		deleteBtn.setImage(IconLoader.loadIcon("table_row_delete"));
