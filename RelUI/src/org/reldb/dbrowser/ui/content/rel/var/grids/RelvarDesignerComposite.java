9a557db8ee42c1e951056e0b4447a7b57eaeba9c
dave <dave@armchair.mb.ca>
2015-08-22 13:11:28 +0100

    Improved command-line interface in relvar designer.

--- RelUI/src/org/reldb/dbrowser/ui/content/rel/var/grids/RelvarDesignerComposite.java
+++ RelUI/src/org/reldb/dbrowser/ui/content/rel/var/grids/RelvarDesignerComposite.java
@@ -2,21 +2,15 @@ package org.reldb.dbrowser.ui.content.rel.var.grids;
 
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.custom.SashForm;
-import org.eclipse.swt.events.SelectionAdapter;
-import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.FillLayout;
-import org.eclipse.swt.layout.FormLayout;
-import org.eclipse.swt.widgets.Button;
-import org.eclipse.swt.layout.FormData;
-import org.eclipse.swt.layout.FormAttachment;
-import org.eclipse.swt.widgets.Text;
 import org.reldb.dbrowser.ui.DbConnection;
+import org.reldb.dbrowser.ui.content.cmd.CmdPanel;
 
 public class RelvarDesignerComposite extends Composite {
 	private RelvarDesigner relvarDesigner;
-	private Text textActions;
-	private Button btnApply;
+	private CmdPanel cmdPanel;
 
 	/**
 	 * Create the composite.
@@ -25,6 +19,7 @@ public class RelvarDesignerComposite extends Composite {
 	 */
 	public RelvarDesignerComposite(Composite parent, DbConnection connection, String relvarName) {
 		super(parent, SWT.NONE);
+		
 		setLayout(new FillLayout(SWT.HORIZONTAL));
 		
 		SashForm sashForm = new SashForm(this, SWT.NONE);
@@ -34,35 +29,22 @@ public class RelvarDesignerComposite extends Composite {
 		relvarDesigner = new RelvarDesigner(designerSurround, connection, relvarName) {
 			protected void changedDefinition() {
 				String relDefinition = relvarDesigner.getRelDefinition();
-				textActions.setText(relDefinition);
-				btnApply.setEnabled(relDefinition.length() > 0);		
+				cmdPanel.setInputText(relDefinition);
+				cmdPanel.setEnabled(relDefinition.length() > 0);
 			}
 		};
-		
-		Composite outputPanel = new Composite(sashForm, SWT.BORDER);
-		outputPanel.setLayout(new FormLayout());
-		
-		btnApply = new Button(outputPanel, SWT.NONE);
-		btnApply.setEnabled(false);
-		FormData fd_btnApply = new FormData();
-		fd_btnApply.left = new FormAttachment(0, 5);
-		fd_btnApply.bottom = new FormAttachment(100, -5);
-		btnApply.setLayoutData(fd_btnApply);
-		btnApply.setText("Apply");
-		btnApply.addSelectionListener(new SelectionAdapter() {
-			@Override
-			public void widgetSelected(SelectionEvent e) {
-				
-			}
-		});
-		
-		textActions = new Text(outputPanel, SWT.BORDER | SWT.H_SCROLL | SWT.V_SCROLL);
-		FormData fd_textActions = new FormData();
-		fd_textActions.top = new FormAttachment(0, 5);
-		fd_textActions.left = new FormAttachment(0, 5);
-		fd_textActions.bottom = new FormAttachment(btnApply, -5);
-		fd_textActions.right = new FormAttachment(100, -5);
-		textActions.setLayoutData(fd_textActions);
+
+		try {
+			cmdPanel = new CmdPanel(connection, sashForm, CmdPanel.NO_INPUT_TOOLBAR) {
+				public void notifyExecuteSuccess() {
+					relvarDesigner.refresh();
+				}
+			};
+			cmdPanel.setEnabled(false);
+		} catch (Exception e) {
+			e.printStackTrace();
+			MessageDialog.openError(getShell(), "Error", "Unable to open database connection:\n\n" + e.getMessage());
+		}
 		
 		sashForm.setWeights(new int[] {8, 3});
 	}
