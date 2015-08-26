737262172e92fd4a899eddc60745756ba23f7391
dave <dave@armchair.mb.ca>
2015-08-26 20:55:28 +0100

    VAR drop and create added.

--- RelUI/src/org/reldb/dbrowser/ui/content/rel/var/VarRealCreator.java
+++ RelUI/src/org/reldb/dbrowser/ui/content/rel/var/VarRealCreator.java
@@ -2,6 +2,7 @@ package org.reldb.dbrowser.ui.content.rel.var;
 
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.swt.custom.CTabItem;
+import org.reldb.dbrowser.ui.DbConnection;
 import org.reldb.dbrowser.ui.content.rel.DbTreeAction;
 import org.reldb.dbrowser.ui.content.rel.DbTreeItem;
 import org.reldb.dbrowser.ui.content.rel.NewItemDialog;
@@ -20,11 +21,18 @@ public class VarRealCreator extends DbTreeAction {
 		NewItemDialog namer = new NewItemDialog(relPanel.getShell(), "Variable" + database.getUniqueNumber());
 		if (namer.open() != NewItemDialog.OK)
 			return;
-		if (database.relvarExists(namer.getName())) {
-			MessageDialog.openInformation(relPanel.getShell(), "Note", "A variable named " + namer.getName() + " already exists.");
+		String varname = namer.getName();
+		if (database.relvarExists(varname)) {
+			MessageDialog.openInformation(relPanel.getShell(), "Note", "A variable named " + varname + " already exists.");
 			return;
 		}
-		DbTreeItem newItem = new DbTreeItem(item, namer.getName());
+		DbConnection.ExecuteResult result = relPanel.getConnection().execute("VAR " + varname + " REAL RELATION {} KEY {};");
+		if (result.failed()) {
+			MessageDialog.openError(relPanel.getShell(), "Error", "Unable to create var " + varname + ": " + result.getErrorMessage());
+			return;
+		}
+		relPanel.redisplayed();
+		DbTreeItem newItem = new DbTreeItem(item, varname);
 		CTabItem tab = relPanel.getTab(newItem);
 		if (tab != null)
 			tab.dispose();
--- RelUI/src/org/reldb/dbrowser/ui/content/rel/var/VarRealDropper.java
+++ RelUI/src/org/reldb/dbrowser/ui/content/rel/var/VarRealDropper.java
@@ -1,5 +1,8 @@
 package org.reldb.dbrowser.ui.content.rel.var;
 
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.swt.custom.CTabItem;
+import org.reldb.dbrowser.ui.DbConnection;
 import org.reldb.dbrowser.ui.content.rel.DbTreeAction;
 import org.reldb.dbrowser.ui.content.rel.DbTreeItem;
 import org.reldb.dbrowser.ui.content.rel.RelPanel;
@@ -12,8 +15,19 @@ public class VarRealDropper extends DbTreeAction {
 
 	@Override
 	public void go(DbTreeItem item) {
-		// TODO Auto-generated method stub
-
+		CTabItem tab = relPanel.getTab(item.getTabName());
+		if (tab != null) {
+			relPanel.getTabFolder().setSelection(tab);
+			MessageDialog.openInformation(relPanel.getShell(), "Note", "You must close the '" + item.getTabName() + "' tab first.");
+		} else {
+			if (!MessageDialog.openConfirm(relPanel.getShell(), "Confirm DROP", "Are you sure you wish to drop var " + item.getName() + "?"))
+				return;
+			DbConnection.ExecuteResult result = relPanel.getConnection().execute("DROP VAR " + item.getName() + ";");
+			if (result.failed())
+				MessageDialog.openError(relPanel.getShell(), "Error", "Unable to drop var " + item.getName() + ": " + result.getErrorMessage());
+			else
+				relPanel.redisplayed();
+		}
 	}
 
 }
