d849ec5a2de27dacd9f3db4b59523a0a4912d3d6
Dave Voorhis <dave@armchair.mb.ca>
2016-06-28 17:13:59 +0100

    Improve post product-build setup script.

--- _Deployment/productPostBuild.sh
+++ _Deployment/productPostBuild.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-#cp -R ~/Documents/JavaJREs/jre-8u40-linux-i586 ~/git/Rel/_Deployment/product/linux.gtk.x86/jre
-#cp -R ~/Documents/JavaJREs/jre-8u40-linux-x64 ~/git/Rel/_Deployment/product/linux.gtk.x86_64/jre
-cp -R ~/Documents/JavaJREs/jre-8u40-macosx-x64/Contents/Home ~/git/Rel/_Deployment/product/macosx.cocoa.x86_64/Rel.app/Contents/MacOS/jre
+version=8u92
+#cp -R ~/Documents/JavaJREs/jre-$version-linux-i586 ~/git/Rel/_Deployment/product/linux.gtk.x86/Rel.app/jre
+#cp -R ~/Documents/JavaJREs/jre-$version-linux-x64 ~/git/Rel/_Deployment/product/linux.gtk.x86_64/Rel.app/jre
+cp -R ~/Documents/JavaJREs/jre-$version-macosx-x64/Contents/Home ~/git/Rel/_Deployment/product/macosx.cocoa.x86_64/Rel.app/Contents/MacOS/jre
 xsltproc -o ~/git/Rel/_Deployment/tmp.plist ~/git/Rel/_Deployment/productMacOS_updatePlist.xslt ~/git/Rel/_Deployment/product/macosx.cocoa.x86_64/Rel.app/Contents/Info.plist
 mv ~/git/Rel/_Deployment/tmp.plist ~/git/Rel/_Deployment/product/macosx.cocoa.x86_64/Rel.app/Contents/Info.plist
-#cp -R ~/Documents/JavaJREs/jre-8u40-windows-i586 ~/git/Rel/_Deployment/product/win32.win32.x86/jre
-#cp -R ~/Documents/JavaJREs/jre-8u40-windows-x64 ~/git/Rel/_Deployment/product/win32.win32.x86_64/jre
+#cp -R ~/Documents/JavaJREs/jre-$version-windows-i586 ~/git/Rel/_Deployment/product/win32.win32.x86/Rel.app/jre
+#cp -R ~/Documents/JavaJREs/jre-$version-windows-x64 ~/git/Rel/_Deployment/product/win32.win32.x86_64/Rel.app/jre
