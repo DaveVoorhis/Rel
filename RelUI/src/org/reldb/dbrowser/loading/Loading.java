f101d477b1b4305f21444cd97aad7a9c67530aa5
dave <dave@armchair.mb.ca>
2016-09-22 07:15:31 +0100

    Aesthetically improved Loading window.

--- RelUI/src/org/reldb/dbrowser/loading/Loading.java
+++ RelUI/src/org/reldb/dbrowser/loading/Loading.java
@@ -1,8 +1,7 @@
 package org.reldb.dbrowser.loading;
 
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.custom.StyleRange;
-import org.eclipse.swt.custom.StyledText;
+import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.graphics.Rectangle;
 import org.eclipse.swt.layout.FormAttachment;
@@ -13,10 +12,15 @@ import org.eclipse.swt.widgets.Monitor;
 import org.eclipse.swt.widgets.ProgressBar;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.wb.swt.SWTResourceManager;
+import org.reldb.dbrowser.ui.IconLoader;
+import org.reldb.dbrowser.utilities.FontSize;
 
 public class Loading {
 
 	private static final int expectedMessageCount = 6;
+
+	private static final int backgroundWidth = 600;
+	private static final int backgroundHeight = 183;
 	
 	private static Loading loading = null;
 	
@@ -67,44 +71,43 @@ public class Loading {
 	
 	private Shell createLoadingShell() {
 		final Shell shell = new Shell(SWT.TOOL | SWT.NO_TRIM);
-		shell.setBackgroundMode(SWT.INHERIT_DEFAULT);
 
 		shell.setLayout(new FormLayout());
+		shell.setMinimumSize(backgroundWidth, backgroundHeight);
+		shell.setSize(backgroundWidth, backgroundHeight);
 		
-		StyledText lblTitle = new StyledText(shell, SWT.NONE);
-		lblTitle.setFont(SWTResourceManager.getFont(".SF NS Text", 16, SWT.NORMAL));
-		FormData fd_lblNewLabel = new FormData();
-		fd_lblNewLabel.top = new FormAttachment(0, 10);
-		fd_lblNewLabel.left = new FormAttachment(0, 10);
-		lblTitle.setLayoutData(fd_lblNewLabel);
-		lblTitle.setText("Rel is loading.");
-		StyleRange italic = new StyleRange();
-		italic.start = 0;
-		italic.length = 3;
-		italic.fontStyle = SWT.ITALIC;
-		lblTitle.setStyleRange(italic);
-		lblTitle.setBackground(shell.getBackground());
+		Image background = IconLoader.loadIconNormal("loading");
+		shell.setBackgroundImage(background);
 		
-		lblAction = new Label(shell, SWT.NONE);
-		FormData fd_lblAction = new FormData();
-		fd_lblAction.top = new FormAttachment(lblTitle, 6);
-		fd_lblAction.left = new FormAttachment(0, 10);
-		lblAction.setLayoutData(fd_lblAction);
-		lblAction.setText("Starting.");
+		Label lblTitle = new Label(shell, SWT.NONE);
+		lblTitle.setFont(FontSize.getThisFontInNewSize(lblTitle.getFont(), 24, SWT.BOLD));
+		lblTitle.setForeground(SWTResourceManager.getColor(SWT.COLOR_WHITE));
+		FormData fd_lblTitle = new FormData();
+		fd_lblTitle.top = new FormAttachment(0, 0);
+		fd_lblTitle.left = new FormAttachment(0, 10);
+		fd_lblTitle.right = new FormAttachment(100, -10);
+		lblTitle.setLayoutData(fd_lblTitle);
+		lblTitle.setText("Loading");
 		
-		progressBar = new ProgressBar(shell, SWT.HORIZONTAL);
-		fd_lblAction.right = new FormAttachment(progressBar, 0, SWT.RIGHT);
+		progressBar = new ProgressBar(shell, SWT.NONE);
+		progressBar.setMaximum(expectedMessageCount);
 		FormData fd_progressBar = new FormData();
-		fd_progressBar.top = new FormAttachment(lblAction, 6);
+		fd_progressBar.bottom = new FormAttachment(100, -10);
 		fd_progressBar.left = new FormAttachment(0, 10);
 		fd_progressBar.right = new FormAttachment(100, -10);
 		progressBar.setLayoutData(fd_progressBar);
-		progressBar.setMaximum(expectedMessageCount);
 		
-		shell.pack();
-		shell.setMinimumSize(600, shell.getSize().y + 20);
-		shell.pack();
+		lblAction = new Label(shell, SWT.WRAP);
+		lblAction.setForeground(SWTResourceManager.getColor(SWT.COLOR_WHITE));
+		FormData fd_lblAction = new FormData();
+		fd_lblAction.top = new FormAttachment(progressBar, -60);
+		fd_lblAction.bottom = new FormAttachment(progressBar, -10);
+		fd_lblAction.left = new FormAttachment(0, 10);
+		fd_lblAction.right = new FormAttachment(100, -200);
+		lblAction.setLayoutData(fd_lblAction);
+		lblAction.setText("Starting...");
 		
+		shell.setSize(background.getBounds().x, background.getBounds().y);
 		shell.setLocation(getMonitorCenter(shell));
 		return shell;
 	}
