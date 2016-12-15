c711493150e8d8dde7449a3adf3380739a246a62
dave <dave@armchair.mb.ca>
2016-12-15 22:50:48 +0000

    Fix formatting.

--- ServerV0000/src/org/reldb/rel/v0/interpreter/ClassPathHack.java
+++ ServerV0000/src/org/reldb/rel/v0/interpreter/ClassPathHack.java
@@ -10,7 +10,7 @@ import java.net.*;
 public class ClassPathHack {
 
 	@SuppressWarnings("rawtypes")
-	private static final Class[] parameters = new Class[]{URL.class};
+	private static final Class[] parameters = new Class[] {URL.class};
 	
 	public static void addFile(String s) throws IOException {
 		File f = new File(s);
@@ -24,19 +24,17 @@ public class ClassPathHack {
 	
 	@SuppressWarnings("unchecked")
 	public static void addURL(URL u) throws IOException {
-			
 		URLClassLoader sysloader = (URLClassLoader)ClassLoader.getSystemClassLoader();
 		@SuppressWarnings("rawtypes")
-		Class sysclass = URLClassLoader.class;
-	
+		Class sysclass = URLClassLoader.class;	
 		try {
-			Method method = sysclass.getDeclaredMethod("addURL",parameters);
+			Method method = sysclass.getDeclaredMethod("addURL", parameters);
 			method.setAccessible(true);
-			method.invoke(sysloader,new Object[]{ u });
+			method.invoke(sysloader, new Object[] {u});
 		} catch (Throwable t) {
-				t.printStackTrace();
-				throw new IOException("Error, could not add URL to system classloader");
-		}			
+			t.printStackTrace();
+			throw new IOException("Error, could not add URL to system classloader");
+		}		
 	}
-		
+
 }
