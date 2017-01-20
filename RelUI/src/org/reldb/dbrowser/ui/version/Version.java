e4adb868ba512fa4412d43a0547c6a0a9a14efe6
dave <dave@armchair.mb.ca>
2017-01-20 17:50:29 +0000

    Increment version numbers after release.

--- RelUI/src/org/reldb/dbrowser/ui/version/Version.java
+++ RelUI/src/org/reldb/dbrowser/ui/version/Version.java
@@ -3,7 +3,7 @@ package org.reldb.dbrowser.ui.version;
 public class Version {
 	
 	public static double getVersionNumber() {
-		return 3.004;
+		return 3.005;
 	}
 	
 	public static String getVersion() {
--- _Deployment/productPostBuild.sh
+++ _Deployment/productPostBuild.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-relversion=3.004
+relversion=3.005
 javaversion=8u111
 jredir=~/Documents/JavaJREs
 proddir=~/git/Rel/_Deployment/product
