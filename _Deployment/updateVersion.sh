aa5e88fcf68549fa97f41b26982d3938496e7647
dave <dave@armchair.mb.ca>
2017-02-27 17:49:08 +0000

    Provide version updating script

---
+++ _Deployment/updateVersion.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+olddbrowserVersion=3.006
+newdbrowserVersion=3.007
+
+oldPRODUCT_VERSION=1
+oldDATABASE_VERSION=0
+oldREVISION=19
+oldRELEASE=Beta
+
+newPRODUCT_VERSION=1
+newDATABASE_VERSION=0
+newREVISION=20
+newRELEASE=Beta
+
+oldRelV=$oldPRODUCT_VERSION.$oldDATABASE_VERSION.$oldREVISION
+newRelV=$newPRODUCT_VERSION.$newDATABASE_VERSION.$newREVISION
+
+oldRelVersion=$oldRelV.$oldRELEASE
+newRelVersion=$newRelV.$newRELEASE
+
+sed -i '' -e "s/$oldRelVersion/$newRelVersion/" ../RelUI/Rel.product
+
+sed -i '' -e "s/return $olddbrowserVersion;/return $newdbrowserVersion;/" ../RelUI/src/org/reldb/dbrowser/ui/version/Version.java
+
+sed -i '' -e "s/PRODUCT_VERSION = $oldPRODUCT_VERSION/PRODUCT_VERSION = $newPRODUCT_VERSION/" ../ServerV0000/src/org/reldb/rel/v0/version/Version.java
+sed -i '' -e "s/DATABASE_VERSION = $oldDATABASE_VERSION/DATABASE_VERSION = $newDATABASE_VERSION/" ../ServerV0000/src/org/reldb/rel/v0/version/Version.java
+sed -i '' -e "s/REVISION = $oldREVISION/REVISION = $newREVISION/" ../ServerV0000/src/org/reldb/rel/v0/version/Version.java
+sed -i '' -e "s/RELEASE = $oldRELEASE/RELEASE = $newRELEASE/" ../ServerV0000/src/org/reldb/rel/v0/version/Version.java
+
+sed -i '' -e "s/relversion=$olddbrowserVersion/relversion=$newdbrowserVersion/" ../_Deployment/productPostBuild.sh
