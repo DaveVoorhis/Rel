99013abb7b99e21d1c517671a65b7f2023fb9406
dave <dave@armchair.mb.ca>
2017-10-24 10:29:19 +0100

    Fix formatting.

--- Client/org/reldb/rel/client/connection/stream/ClientFromURL.java
+++ Client/org/reldb/rel/client/connection/stream/ClientFromURL.java
@@ -10,25 +10,28 @@ import org.reldb.rel.shared.Defaults;
 
 public class ClientFromURL {
 
-    /** Open a connection. */
-    public static StreamReceiverClient openConnection(String databaseURL, boolean createDbAllowed, CrashHandler crashHandler, String[] additionalJars) throws NumberFormatException, IOException, MalformedURLException, DatabaseFormatVersionException {
-    	if (databaseURL.toLowerCase().startsWith("db:"))
-    		if (databaseURL.length() > 3)
-    			return new ClientLocalConnection(databaseURL.substring(3).trim(), createDbAllowed, crashHandler, additionalJars);
-    		else
-    			throw new MalformedURLException("Please specify a local database as db:<directory>");
-    	else {
-    		ClassPathHack.addFile("lib/relshared.jar");
-        	String hostName = databaseURL;
-        	int port = Defaults.getDefaultPort();
-        	int colonPosition = databaseURL.indexOf(':');
-        	if (colonPosition >= 0) {
-        		hostName = databaseURL.substring(0, colonPosition);
-        		String portString = databaseURL.substring(colonPosition + 1);
-       			port = Integer.parseInt(portString);
-        	}
-        	return new ClientNetworkConnection(hostName, port);
-    	}
-    }
+	/** Open a connection. */
+	public static StreamReceiverClient openConnection(String databaseURL, boolean createDbAllowed,
+			CrashHandler crashHandler, String[] additionalJars)
+			throws NumberFormatException, IOException, MalformedURLException, DatabaseFormatVersionException {
+		if (databaseURL.toLowerCase().startsWith("db:"))
+			if (databaseURL.length() > 3)
+				return new ClientLocalConnection(databaseURL.substring(3).trim(), createDbAllowed, crashHandler,
+						additionalJars);
+			else
+				throw new MalformedURLException("Please specify a local database as db:<directory>");
+		else {
+			ClassPathHack.addFile("lib/relshared.jar");
+			String hostName = databaseURL;
+			int port = Defaults.getDefaultPort();
+			int colonPosition = databaseURL.indexOf(':');
+			if (colonPosition >= 0) {
+				hostName = databaseURL.substring(0, colonPosition);
+				String portString = databaseURL.substring(colonPosition + 1);
+				port = Integer.parseInt(portString);
+			}
+			return new ClientNetworkConnection(hostName, port);
+		}
+	}
 
 }
