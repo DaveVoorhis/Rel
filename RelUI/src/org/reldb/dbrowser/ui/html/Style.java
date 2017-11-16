8359f840ad1738d5978cc35301dd6ec8795649ac
dave <dave@armchair.mb.ca>
2017-11-16 10:52:26 +0000

    Fix CSS for table heading text.

--- RelUI/src/org/reldb/dbrowser/ui/html/Style.java
+++ RelUI/src/org/reldb/dbrowser/ui/html/Style.java
@@ -15,15 +15,15 @@ public class Style {
 	}
 	
 	private static final String[] formattedStyle = {
-		"body {margin-top: 0px; margin-left: 0px; margin-right: 0px; margin-bottom: 0px}",
-		"table {border-style: none; border-width: 0px;}",
-		"td, tr, th {border-style: solid; border-width: 1px;}",
-		".ok {color: green;}",
-	    ".bad {color: red;}",
-	    ".note {color: blue;}",
-	    ".user {color: gray;}",
-	    ".warn {color: gold;}",
-	    ".notice {color: black;}",
+		"body {margin-top: 0px; margin-left: 0px; margin-right: 0px; margin-bottom: 0px}\n",
+		"table {border-style: none; border-width: 0px;}\n",
+		"td, tr, th {border-style: solid; border-width: 1px;}\n",
+		".ok {color: green;}\n",
+	    ".bad {color: red;}\n",
+	    ".note {color: blue;}\n",
+	    ".user {color: gray;}\n",
+	    ".warn {color: gold;}\n",
+	    ".notice {color: black;}\n",
 	};
 	
 	private String getBodyFontStyleString() {
@@ -31,7 +31,7 @@ public class Style {
 		FontData datum = data[0];
 		// eliminate leading '.', if there is one
 		String fontName = (datum.getName().startsWith(".") ? datum.getName().substring(1) : datum.getName());
-		return "body, p, td {font-family: " + fontName + ", sans-serif; font-size: " + (datum.getHeight() + sizeAdjustment) + "pt;}";
+		return "body, p, td, th {font-family: " + fontName + ", sans-serif; font-size: " + (datum.getHeight() + sizeAdjustment) + "pt;}\n";
 	}
 
 	private String getHTMLStyle() {
@@ -43,18 +43,18 @@ public class Style {
 	}
 
 	public String getHTMLDocument(String content) {
-		return	 "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">" +
-			     "<html xmlns=\"http://www.w3.org/1999/xhtml\" dir=\"ltr\" lang=\"en-gb\" xml:lang=\"en-gb\">" +
-			     "<head>" +
-			     "<style type=\"text/css\">" +
-			     "<!--" +
+		return	 "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">\n" +
+			     "<html xmlns=\"http://www.w3.org/1999/xhtml\" dir=\"ltr\" lang=\"en-gb\" xml:lang=\"en-gb\">\n" +
+			     "<head>\n" +
+			     "<style type=\"text/css\">\n" +
+			     "<!--\n" +
 			     getHTMLStyle() +
-				 "-->" +
-				 "</style>" +
-				 "</head>" +
-				 "<body id=\"body\">" +
+				 "-->\n" +
+				 "</style>\n" +
+				 "</head>\n" +
+				 "<body id=\"body\">\n" +
 				 content +
-				 "</body>" +
+				 "</body>\n" +
 				 "</html>";
 	}
 	
