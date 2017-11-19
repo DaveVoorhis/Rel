11f0306e2f963fb3d86e34bf47cafec2042f106d
dave <dave@armchair.mb.ca>
2017-11-19 21:48:23 +0000

    Fix formatting.

--- RelUI/src/org/reldb/dbrowser/ui/content/rev/Model.java
+++ RelUI/src/org/reldb/dbrowser/ui/content/rev/Model.java
@@ -21,96 +21,97 @@ import org.reldb.dbrowser.ui.RevDatabase;
 /**
  * Defines a layered pane in which visualised classes may be manipulated.
  *
- * @author  Dave Voorhis
+ * @author Dave Voorhis
  */
 public class Model extends Composite {
-    
+
 	private int lastMouseX;
 	private int lastMouseY;
-	
+
 	private Rev rev;
-	
+
 	private String modelName;
-	
-    /** Ctor */
-    public Model(Rev rev, String modelName, Composite parent) {
-    	super(parent, SWT.NONE);
-    	
-    	this.rev = rev;
-    	this.modelName = modelName;
-    	
-    	addMouseMoveListener(new MouseMoveListener() {
+
+	/** Ctor */
+	public Model(Rev rev, String modelName, Composite parent) {
+		super(parent, SWT.NONE);
+
+		this.rev = rev;
+		this.modelName = modelName;
+
+		addMouseMoveListener(new MouseMoveListener() {
 			@Override
 			public void mouseMove(MouseEvent e) {
 				lastMouseX = e.x;
 				lastMouseY = e.y;
 				enablePopupMenu();
 			}
-    	});
-    }
+		});
+	}
 
-    private Menu preservedPopupMenu;
-    
-    void disablePopupMenu() {
-    	if (getMenu() == null)
-    		return;
-    	preservedPopupMenu = getMenu();
-    	setMenu(null);
-    }
-    
-    void enablePopupMenu() {
-    	if (getMenu() != null)
-    		return;
-    	if (preservedPopupMenu == null)
-    		return;
-    	setMenu(preservedPopupMenu);
-    	preservedPopupMenu = null;
-    }
-    
-    public Point getLastMousePosition() {
-    	return new Point(lastMouseX, lastMouseY);
-    }
-    
+	private Menu preservedPopupMenu;
+
+	void disablePopupMenu() {
+		if (getMenu() == null)
+			return;
+		preservedPopupMenu = getMenu();
+		setMenu(null);
+	}
+
+	void enablePopupMenu() {
+		if (getMenu() != null)
+			return;
+		if (preservedPopupMenu == null)
+			return;
+		setMenu(preservedPopupMenu);
+		preservedPopupMenu = null;
+	}
+
+	public Point getLastMousePosition() {
+		return new Point(lastMouseX, lastMouseY);
+	}
+
 	public void removeEverything() {
 		Collection<Visualiser> visualisers = new HashSet<Visualiser>();
-		for (Control child: getChildren())
+		for (Control child : getChildren())
 			if (child instanceof Visualiser)
-				visualisers.add((Visualiser)child);
-		for (Visualiser visualiser: visualisers)
+				visualisers.add((Visualiser) child);
+		for (Visualiser visualiser : visualisers)
 			visualiser.delete();
 		rev.fireModelChangeEvent();
 	}
 
 	void clear() {
-		for (Control control: getChildren())
+		for (Control control : getChildren())
 			control.dispose();
 	}
 
 	public int getVisualiserCount() {
 		int count = 0;
-		for (Control child: getChildren())
+		for (Control child : getChildren())
 			if (child instanceof Visualiser)
 				count++;
 		return count;
 	}
-	
+
 	public Visualiser getVisualiser(String visualiserName) {
-		for (Control child: getChildren()) {
+		for (Control child : getChildren()) {
 			if (!(child instanceof Visualiser))
 				continue;
-			Visualiser childVisualiser = (Visualiser)child;
+			Visualiser childVisualiser = (Visualiser) child;
 			if (childVisualiser.getID().compareTo(visualiserName) == 0)
 				return childVisualiser;
 		}
 		return null;
 	}
-	
+
 	public Visualiser getPossibleDropTarget(int i, int j, Visualiser visualiser) {
-		for (Control child: getChildren()) {
+		for (Control child : getChildren()) {
 			if (!(child instanceof Visualiser))
 				continue;
-			Visualiser childVisualiser = (Visualiser)child;
-			if (visualiser.getBounds().intersects(childVisualiser.getBounds()) && childVisualiser.canReceiveDropOf(visualiser))
+			Visualiser childVisualiser = (Visualiser) child;
+			if (visualiser.getBounds().intersects(childVisualiser.getBounds())
+					&& childVisualiser.canReceiveDropOf(visualiser))
 				return childVisualiser;
 		}
 		return null;
@@ -127,9 +128,9 @@ public class Model extends Composite {
 	public void setModelName(String name) {
 		modelName = name;
 	}
-	
+
 	public Rev getRev() {
 		return rev;
 	}
-    
+
 }
