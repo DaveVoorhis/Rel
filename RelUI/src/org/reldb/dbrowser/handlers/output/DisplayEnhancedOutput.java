63f18986597f18c1e1368b35ec4947c3123c26c3
dave <dave@armchair.mb.ca>
2017-11-28 18:33:07 +0000

    Work on output menu.

---
+++ RelUI/src/org/reldb/dbrowser/handlers/output/DisplayEnhancedOutput.java
@@ -0,0 +1,30 @@
+/*******************************************************************************
+ * Copyright (c) 2010 - 2013 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *     Lars Vogel <lars.Vogel@gmail.com> - Bug 419770
+ *******************************************************************************/
+package org.reldb.dbrowser.handlers.output;
+
+import org.eclipse.e4.core.di.annotations.CanExecute;
+import org.eclipse.e4.core.di.annotations.Execute;
+import org.eclipse.e4.ui.model.application.ui.menu.MDirectMenuItem;
+import org.eclipse.swt.widgets.Shell;
+
+public class DisplayEnhancedOutput {
+
+	@CanExecute
+	public boolean canExecute() {
+		return true;
+	}
+	
+	@Execute
+	public void execute(Shell shell, MDirectMenuItem me) {
+		me.setSelected(!me.isSelected());
+	}
+}
