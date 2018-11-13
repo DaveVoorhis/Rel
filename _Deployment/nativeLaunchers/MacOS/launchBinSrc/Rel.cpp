a9495fc3bf67c67f0ac2650e9badb011ba0bdb5f
dave <dave@armchair.mb.ca>
2018-11-13 20:52:48 +0000

    MacOS native launcher now uses Rel.ini

--- _Deployment/nativeLaunchers/MacOS/launchBinSrc/Rel.cpp
+++ _Deployment/nativeLaunchers/MacOS/launchBinSrc/Rel.cpp
@@ -3,9 +3,27 @@
 #include <iostream>
 #include <cstdlib>
 #include <unistd.h>
+#include <libgen.h>
+#include <string>
+#include <fstream>
+#include <streambuf>
 
 int main(int argc, char **argv)
 {
-	chdir(argv[0]);
-	system("jre/bin/java -splash:lib/Splash.png -cp \"lib/*:lib/nattable/*:lib/swt/*:lib/swt/macos_64/*\" -XstartOnFirstThread DBrowser ");
+  // Convert first argument of argv[0] (full pathspec to this executable) to path where executable is found
+  char *dir = dirname(argv[0]);
+  chdir(dir);
+
+  // Read the ini file
+  std::string iniFileName("lib/Rel.ini");
+  std::ifstream configfile(iniFileName);
+  std::string cmd((std::istreambuf_iterator<char>(configfile)), std::istreambuf_iterator<char>());
+
+  // Empty or no ini file?
+  if (cmd.length() == 0) {
+    std::cerr << (std::string("Missing or Damaged .ini File: Unable to find ") + iniFileName).c_str() << std::endl;
+    return 10;
+  }
+
+  return system(cmd.c_str());
 }
