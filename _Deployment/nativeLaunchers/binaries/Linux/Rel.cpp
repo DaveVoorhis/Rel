91e795c852febb5dc8f9d12828a8df685f1fa9ab
DaveVoorhis <voorhis@armchair.mb.ca>
2018-11-24 20:00:06 +0000

    Add comments and compile for Linux.

--- _Deployment/nativeLaunchers/binaries/Linux/Rel.cpp
+++ _Deployment/nativeLaunchers/binaries/Linux/Rel.cpp
@@ -25,6 +25,7 @@ int main(int argc, char **argv)
     return 10;
   }
 
+  // Include command-line args.
   std::string args("");
   for (int i = 1; i < argc; i++)
   	args += std::string(" ") + std::string(argv[i]);
