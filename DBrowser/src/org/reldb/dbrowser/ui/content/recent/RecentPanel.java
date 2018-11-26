4bd86a2114ad416a7a7c59c0076d789c25341ffb
dave <dave@armchair.mb.ca>
2018-11-26 23:19:47 +0000

    Work on recently-used database panel.

--- DBrowser/src/org/reldb/dbrowser/ui/content/recent/RecentPanel.java
+++ DBrowser/src/org/reldb/dbrowser/ui/content/recent/RecentPanel.java
@@ -1,12 +1,14 @@
 package org.reldb.dbrowser.ui.content.recent;
 
+import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.layout.FormLayout;
+import org.eclipse.swt.layout.RowLayout;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.layout.FormData;
-import org.eclipse.swt.layout.FormAttachment;
+import org.eclipse.swt.custom.ScrolledComposite;
+import org.reldb.dbrowser.Core;
 import org.reldb.dbrowser.ui.DbTab;
+import org.reldb.dbrowser.ui.IconLoader;
 
 public class RecentPanel extends Composite {
 
@@ -18,17 +20,30 @@ public class RecentPanel extends Composite {
 	 */
 	public RecentPanel(Composite parent, DbTab dbTab, int style) {
 		super(parent, style);
-		FormLayout formLayout = new FormLayout();
-		formLayout.marginWidth = 5;
-		formLayout.marginHeight = 5;
-		setLayout(formLayout);
-
-		Label lblConvert = new Label(this, SWT.NONE);
-		FormData fd_lblConvert = new FormData();
-		fd_lblConvert.top = new FormAttachment(0);
-		fd_lblConvert.left = new FormAttachment(0);
-		fd_lblConvert.right = new FormAttachment(100);
-		lblConvert.setLayoutData(fd_lblConvert);
-		lblConvert.setText("Recently-used database options go here.");
+		RowLayout rowLayout = new RowLayout();
+		rowLayout.wrap = true;
+		rowLayout.type = SWT.HORIZONTAL;
+		setLayout(rowLayout);
+		
+		Composite newDb = new Composite(this, SWT.BORDER);
+		newDb.setLayout(new RowLayout());
+		Button newDBIcon = new Button(newDb, SWT.NONE);
+		newDBIcon.addListener(SWT.Selection, e -> Core.newDatabase());
+		newDBIcon.setBackgroundImage(IconLoader.loadIcon("large_database_create"));
+		newDBIcon.setText("Create database");
+		
+		for (String dbURL: Core.getRecentlyUsedDatabaseList()) {
+			Composite openDb = new Composite(this, SWT.BORDER);
+			openDb.setLayout(new RowLayout());
+			Button openDbIcon = new Button(openDb, SWT.NONE);
+			openDbIcon.addListener(SWT.Selection, e -> Core.openDatabase(dbURL));
+			openDbIcon.setText(dbURL);
+			if (dbURL.startsWith("db:"))
+				openDbIcon.setBackgroundImage(IconLoader.loadIcon("large_database_load"));
+			else
+				openDbIcon.setBackgroundImage(IconLoader.loadIcon("large_remote_database_load"));
+		}
+		
+		pack();
 	}
 }
