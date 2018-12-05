6893b1d0be27ac15872b4a9ae3239c84ac47d3b3
dave <dave@ubuntu>
2018-12-05 04:44:09 -0800

    Avoid Webkit hassle under Linux by only using BrowserSwing.

---
+++ DBrowser/src/org/reldb/dbrowser/ui/preferences/LabelFieldEditor.java
@@ -0,0 +1,52 @@
+package org.reldb.dbrowser.ui.preferences;
+
+import org.eclipse.jface.preference.FieldEditor;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
+
+// From https://github.com/sergeche/eclipse-zencoding/blob/master/src/io/emmet/eclipse/preferences/LabelFieldEditor.java
+public class LabelFieldEditor extends FieldEditor {
+	private Label label;
+
+	// All labels can use the same preference name since they don't
+	// store any preference.
+	public LabelFieldEditor(String value, Composite parent) {
+		super("label", value, parent);
+	}
+
+	// Adjusts the field editor to be displayed correctly
+	// for the given number of columns.
+	protected void adjustForNumColumns(int numColumns) {
+		((GridData) label.getLayoutData()).horizontalSpan = numColumns;
+	}
+
+	// Fills the field editor's controls into the given parent.
+	protected void doFillIntoGrid(Composite parent, int numColumns) {
+		label = getLabelControl(parent);
+
+		GridData gridData = new GridData();
+		gridData.horizontalSpan = numColumns;
+		gridData.horizontalAlignment = GridData.FILL;
+		gridData.grabExcessHorizontalSpace = false;
+		gridData.verticalAlignment = GridData.CENTER;
+		gridData.grabExcessVerticalSpace = false;
+
+		label.setLayoutData(gridData);
+	}
+
+	// Returns the number of controls in the field editor.
+	public int getNumberOfControls() {
+		return 1;
+	}
+
+	// Labels do not persist any preferences, so these methods are empty.
+	protected void doLoad() {
+	}
+
+	protected void doLoadDefault() {
+	}
+
+	protected void doStore() {
+	}
+}
--- DBrowser/src/org/reldb/dbrowser/ui/preferences/PreferencePageDisplay.java
+++ DBrowser/src/org/reldb/dbrowser/ui/preferences/PreferencePageDisplay.java
@@ -2,6 +2,7 @@ package org.reldb.dbrowser.ui.preferences;
 
 import org.eclipse.jface.preference.BooleanFieldEditor;
 import org.eclipse.jface.preference.FieldEditorPreferencePage;
+import org.eclipse.jface.util.Util;
 
 /**
  * This class creates a preference page
@@ -9,17 +10,18 @@ import org.eclipse.jface.preference.FieldEditorPreferencePage;
 public class PreferencePageDisplay extends FieldEditorPreferencePage {
 	// Names for preferences
 	public static final String CMD_BROWSER_SWING = "browser.swing";
-
-	/**
-	 * Constructor
-	 */
+		
 	public PreferencePageDisplay() {
 		setTitle("Display");
 		setDescription("Display settings.");
 	}
 
 	protected void createFieldEditors() {
-		addField(new BooleanFieldEditor(CMD_BROWSER_SWING, "Use an alternative output display to try to fix output problems.", getFieldEditorParent()));
+		if (!Util.isGtk())
+			addField(new BooleanFieldEditor(CMD_BROWSER_SWING, "Use an alternative output display to try to fix output problems.", getFieldEditorParent()));
+		else {
+			new LabelFieldEditor("...Not available under Linux.", getFieldEditorParent());
+		}
 	}
 
 }
