ab285939a3777f2e41c7f86f7d46afe08cc9dae7
dave <dave@armchair.mb.ca>
2018-12-06 17:32:28 +0000

    Complete Restore Database facility.

--- DBrowser/src/org/reldb/dbrowser/ui/RestoreDatabaseDialog.java
+++ DBrowser/src/org/reldb/dbrowser/ui/RestoreDatabaseDialog.java
@@ -8,33 +8,48 @@ import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.layout.FormLayout;
 import org.eclipse.swt.widgets.Label;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.nio.file.Files;
 
+import org.apache.poi.Version;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.StyleRange;
+import org.eclipse.swt.custom.StyledText;
+import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.layout.FormData;
 import org.eclipse.swt.layout.FormAttachment;
 import org.eclipse.swt.widgets.Text;
 import org.reldb.dbrowser.Core;
+import org.reldb.dbrowser.ui.crash.CrashTrap;
 import org.reldb.rel.client.Connection;
-import org.reldb.rel.client.Response;
+import org.reldb.rel.client.connection.stream.ClientLocalConnection;
 import org.eclipse.swt.widgets.Button;
 
 public class RestoreDatabaseDialog extends Dialog {
-
+	
+	private enum Mode {CREATEDB, LOADDB};
+	
+	private static Mode mode = Mode.CREATEDB;
+	
 	protected Shell shell;
 	
 	private Text textDatabaseDir;
 	private Text textSourceFile;
-	private Text textOutput;
+	private StyledText textOutput;
 	
 	private DirectoryDialog newDatabaseDialog;
 	private FileDialog restoreFileDialog;
 	
 	private Button btnCancel;
 	private Button btnOk;
+	
+	private Color red;
+	private Color black;
+	private Color green;
 
 	/**
 	 * Create the dialog.
@@ -76,26 +91,90 @@ public class RestoreDatabaseDialog extends Dialog {
 		textDatabaseDir.setEnabled(false);
 		textSourceFile.setEnabled(false);
 		btnOk.setVisible(false);
-		btnCancel.setText("Close");		
+		btnCancel.setText("Close");
+		btnCancel.requestLayout();
+		mode = Mode.CREATEDB;
 	}
 	
+	private void setupUIAsReload() {
+		textDatabaseDir.setEnabled(false);
+		textSourceFile.setEnabled(true);
+		btnOk.setVisible(true);
+		btnOk.setText("Reload");
+		btnOk.requestLayout();
+		mode = Mode.LOADDB;
+	}
+	
+	private void output(String s, Color color) {
+		shell.getDisplay().asyncExec(() -> {
+			StyleRange styleRange = new StyleRange();
+			styleRange.start = textOutput.getCharCount();
+			styleRange.length = s.length();
+			styleRange.fontStyle = SWT.NORMAL;
+			styleRange.foreground = color;
+			textOutput.append(s);
+			textOutput.append("\n");
+			textOutput.setStyleRange(styleRange);
+			textOutput.setCaretOffset(textOutput.getCharCount());
+			textOutput.setSelection(textOutput.getCaretOffset(), textOutput.getCaretOffset());
+		});
+	}
+
+	private boolean loadFail = true;
+	private boolean running = false;
+	private String lastError = "";
+	
 	private void process(String dbURL, String backupToRestore) {
-		textOutput.append("Ready to create database " + dbURL + "\n");
-		try (Connection connection = new Connection(dbURL, true)) {
-			textOutput.append("Database " + dbURL + " created.\n");			
-			try {
-				String serverAnnouncement = connection.getServerAnnouncement();
-				textOutput.append(serverAnnouncement + "\n");		
-				Response response = connection.execute(backupToRestore);
-				response.awaitResult(999999);
+		ClientLocalConnection connection = null;
+		try {
+			connection = new ClientLocalConnection(dbURL, mode == Mode.CREATEDB, new CrashTrap(shell, Version.getVersion()), null);
+			final BufferedReader input = new BufferedReader(new InputStreamReader(connection.getServerResponseInputStream()));
+			Thread thread = new Thread() {
+				public void run() {
+					String r;
+					loadFail = false;
+					running = true;
+					lastError = "";
+					try {
+						while ((r = input.readLine()) != null) {
+							if (r.equals("<EOT>")) {
+								break;
+							} else if (r.startsWith("ERROR:")) {
+								output(r, red);
+								lastError = r;
+								loadFail = true;
+								break;
+							} else if (!r.equals("Ok.")) {
+								output(r, black);
+							}
+							Thread.yield();
+						}
+						running = false;
+					} catch (IOException e) {
+						return;
+					}
+				}
+			};
+			thread.start();
+			connection.sendExecute(backupToRestore);
+			while (running)
+				Thread.yield();
+			if (loadFail) {
+				output("Load failed due to " + lastError, red);
+				setupUIAsReload();
+			} else {
+				output("Done.", green);
 				setupUIAsFinished();
-			} catch (IOException e1) {
-				textOutput.append("Unable to communicate with database due to " + e1.getMessage());
 			}
 		} catch (Exception e) {
-			textOutput.append("Unable to create database " + dbURL + "\n");
-			textOutput.append(e.getMessage());
+			output("Error:\n" + e.getMessage(), red);
 		}
+		if (connection != null)
+			try {
+				connection.close();
+			} catch (IOException e) {
+				output("Problem closing connection: " + e.getMessage(), red);
+			}
 	}
 	
 	/**
@@ -106,6 +185,10 @@ public class RestoreDatabaseDialog extends Dialog {
 		shell.setSize(640, 480);
 		shell.setText(getText());
 		shell.setLayout(new FormLayout());
+		
+		red = new Color(shell.getDisplay(), 200, 0, 0);
+		black = new Color(shell.getDisplay(), 0, 0, 0);
+		green = new Color(shell.getDisplay(), 0, 100, 0);
 		
 		Label lblDatabaseDir = new Label(shell, SWT.NONE);
 		FormData fd_lblDatabaseDir = new FormData();
@@ -144,6 +227,16 @@ public class RestoreDatabaseDialog extends Dialog {
 		FormData fd_btnSourceFile = new FormData();
 		btnSourceFile.setLayoutData(fd_btnSourceFile);
 		btnSourceFile.setText("Choose file...");
+		btnSourceFile.addListener(SWT.Selection, e -> {
+			if (textSourceFile.getText().trim().length() == 0)
+				restoreFileDialog.setFilterPath(System.getProperty("user.home"));
+			else
+				restoreFileDialog.setFilterPath(textSourceFile.getText());
+			String result = restoreFileDialog.open();
+			if (result == null)
+				return;
+			textSourceFile.setText(result);
+		});
 		
 		btnOk = new Button(shell, SWT.NONE);
 		FormData fd_btnOk = new FormData();
@@ -176,18 +269,21 @@ public class RestoreDatabaseDialog extends Dialog {
 				MessageDialog.openInformation(shell, "Unable to Read Backup File", "The database backup file appears to be empty or unreadable.");
 				return;
 			}
-			String dbURL = "db:" + databaseDir;
-			Connection connection;
-			try {
-				connection = new Connection(dbURL);
-				connection.close();
-				MessageDialog.openInformation(shell, "Database Exists", "A Rel database already exists at " + dbURL);
-			} catch (Exception e1) {
-				Throwable cause = e1.getCause();
-				if (cause != null && cause.getMessage().startsWith("RS0406:"))
-					process(dbURL, backup);
-				else
-					MessageDialog.openError(shell, "Problem with Directory", "Unable to use " + dbURL + " due to: " + e1.getMessage());
+			if (mode == Mode.LOADDB) {
+				process(databaseDir, backup);
+			} else {
+				Connection connection;
+				try {
+					connection = new Connection("db:" + databaseDir);
+					connection.close();
+					MessageDialog.openInformation(shell, "Database Exists", "A Rel database already exists at " + databaseDir);
+				} catch (Exception e1) {
+					Throwable cause = e1.getCause();
+					if (cause != null && cause.getMessage().startsWith("RS0406:"))
+						process(databaseDir, backup);
+					else
+						MessageDialog.openError(shell, "Problem with Directory", "Unable to use " + databaseDir + " due to: " + e1.getMessage());
+				}
 			}
 		});
 		
@@ -202,7 +298,9 @@ public class RestoreDatabaseDialog extends Dialog {
 		lblOutput.setLayoutData(fd_lblOutput);
 		lblOutput.setText("Output");
 		
-		textOutput = new Text(shell, SWT.BORDER | SWT.MULTI);
+		textOutput = new StyledText(shell, SWT.BORDER | SWT.V_SCROLL | SWT.MULTI | SWT.H_SCROLL);
+		textOutput.setEditable(false);
+		textOutput.setWordWrap(true);
 		FormData fd_textOutput = new FormData();
 		textOutput.setLayoutData(fd_textOutput);
 
