b340f481da9ac563ff61d410abcecf34b1982eab
dave <dave@armchair.mb.ca>
2020-10-06 22:04:44 +0100

    Remove deprecated and unnecessary ShowRowInViewport command.

--- DBrowser/src/org/reldb/dbrowser/ui/content/rel/var/grids/Editor.java
+++ DBrowser/src/org/reldb/dbrowser/ui/content/rel/var/grids/Editor.java
@@ -64,7 +64,6 @@ import org.eclipse.nebula.widgets.nattable.ui.menu.PopupMenuBuilder;
 import org.eclipse.nebula.widgets.nattable.ui.util.CellEdgeEnum;
 import org.eclipse.nebula.widgets.nattable.util.GCFactory;
 import org.eclipse.nebula.widgets.nattable.util.GUIHelper;
-import org.eclipse.nebula.widgets.nattable.viewport.command.ShowRowInViewportCommand;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.PaintEvent;
 import org.eclipse.swt.events.PaintListener;
@@ -1006,7 +1005,6 @@ public abstract class Editor extends Grid {
 			table.doCommand(new ClearAllSelectionsCommand());
 			if (gridLayer != null && dataProvider != null) {
 				int lastRow = dataProvider.getRowCount() - 1;
-				table.doCommand(new ShowRowInViewportCommand(gridLayer.getBodyLayer().getViewportLayer(), lastRow));
 				table.doCommand(new SelectCellCommand(gridLayer.getBodyLayer().getSelectionLayer(), 0, lastRow, true, true));
 			}
 		}
