4f46a5eaaa392a4af76edbb015cd7b7ada1fe5d6
dave <dave@armchair.mb.ca>
2022-10-05 22:11:58 +0100

    Increment versions after release.

--- DBrowser/src/main/java/org/reldb/dbrowser/ui/version/Version.java
+++ DBrowser/src/main/java/org/reldb/dbrowser/ui/version/Version.java
@@ -3,7 +3,7 @@ package org.reldb.dbrowser.ui.version;
 public class Version {
 	
 	public static double getVersionNumber() {
-		return 3.015;
+		return 3.016;
 	}
 	
 	public static String getVersion() {
